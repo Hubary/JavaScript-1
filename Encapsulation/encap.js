4e7cbe0c40b398064e5b323b44689ff57fdae069
jessie-zly <610141748@qq.com>
2017-10-12 23:19:24 +0800

    add day09

--- Encapsulation/encap.js
+++ Encapsulation/encap.js
@@ -6,7 +6,6 @@
  */
 
 
-
 /**
  * 功能: 不足位数时前置补0
  * @param num 待补位的数
@@ -30,7 +29,6 @@ function addPrefixZero(num, length) {
     }
 }
 
-
 /**
  * 功能: 将时间对象转换成字符串
  * @param date 需要转换的时间对象
@@ -47,3 +45,56 @@ function dateToString(date, devideSign, num) {
     //调用前置位0,进行字符串拼接
     return date.getFullYear() + devideSign + addPrefixZero((date.getMonth() + 1), num) + devideSign + addPrefixZero(date.getDate(), num) + " " + addPrefixZero(date.getHours(), num) + ":" + addPrefixZero(date.getMinutes(), num) + ":" + addPrefixZero(date.getSeconds(), num);
 }
+
+
+/**
+ * 功能: 生成m-n之间的随机整数
+ * @param m 起始数字
+ * @param n 终止数字
+ * @returns {Number}
+ */
+function randomNumM_N(m,n) {
+    //判断m,n是否为非负数
+    if(m<0||n<=0){
+        //提示信息
+        return "请重新输入m,n";
+    }
+    //判断m和n的关系
+    if(m>n){
+        //如果m>n,则交换m和n的值
+        var temp=m;
+        m=n;
+        n=temp;
+    }
+    //返回值
+    return parseInt(m+Math.random()*(n-m+1));
+}
+
+/**
+ * 功能: 颜色值转换为16进制,不足前置补0
+ * @param R 0-255
+ * @param G 0-255
+ * @param B 0-255
+ * @returns {string}
+ */
+function stringToNum(R,G,B) {
+    R=R.toString(16).length<2?"0"+R.toString(16):R.toString(16);
+    G=G.toString(16).length<2?"0"+G.toString(16):G.toString(16);
+    B=B.toString(16).length<2?"0"+B.toString(16):B.toString(16);
+    return R+G+B;
+}
+
+/**
+ * 功能: 随机颜色
+ * @param R 0-255
+ * @param G 0-255
+ * @param B 0-255
+ * @returns {string}
+ */
+function randomColorByRGB(R,G,B) {
+    R=randomNumM_N(0,255);
+    G=randomNumM_N(0,255);
+    B=randomNumM_N(0,255);
+    return "#"+stringToNum(R,G,B);
+}
+
